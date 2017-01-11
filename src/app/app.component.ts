ef31e7d55d8401e8548b9e9a81cc55e58ed69611
OKUNOKENTARO <okunokentaro@me.com>
2017-01-11 16:44:51 +0900

    init an app prefix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core'
 import {AngularFire} from 'angularfire2'
 
 @Component({
-  selector: 'app-root',
+  selector: 'ch-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
