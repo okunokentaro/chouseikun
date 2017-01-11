619d8191f5a6de9a75d1d82c3921c020d3d1b22b
OKUNOKENTARO <okunokentaro@me.com>
2017-01-11 14:13:26 +0900

    install firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser'
-import { NgModule } from '@angular/core'
-import { FormsModule } from '@angular/forms'
-import { HttpModule } from '@angular/http'
+import {BrowserModule} from '@angular/platform-browser'
+import {NgModule} from '@angular/core'
+import {FormsModule} from '@angular/forms'
+import {HttpModule} from '@angular/http'
+import {AngularFireModule} from 'angularfire2'
 
-import { AppComponent } from './app.component'
+import {FIREBASE_CONFIG} from './firebase-config'
+import {AppComponent} from './app.component'
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AngularFireModule.initializeApp(FIREBASE_CONFIG),
     BrowserModule,
     FormsModule,
     HttpModule
---
+++ src/app/firebase-config.ts
@@ -0,0 +1,7 @@
+export const FIREBASE_CONFIG = {
+  apiKey           : 'AIzaSyB4r-1COyvDxQIWWAocrqCp2OrMCOLleGc',
+  authDomain       : 'chouseikun-d59bc.firebaseapp.com',
+  databaseURL      : 'https://chouseikun-d59bc.firebaseio.com',
+  storageBucket    : 'chouseikun-d59bc.appspot.com',
+  messagingSenderId: '1099145559138'
+}
