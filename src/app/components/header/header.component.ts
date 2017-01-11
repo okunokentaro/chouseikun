9735f839c1f726e865cf8eeecffcfeb8966634e1
OKUNOKENTARO <okunokentaro@me.com>
2017-01-11 22:03:13 +0900

    add an avatar image

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -9,7 +9,7 @@ import {AuthService} from '../../services/auth.service'
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  constructor(private auth: AuthService,
+  constructor(public auth: AuthService,
               private router: Router) {}
 
   ngOnInit() {
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -12,16 +12,26 @@ export enum LoginStatus {
 export class AuthService {
   whenLoggedIn = new Subject<any>()
   whenLoggedOut = new Subject<any>()
+
+  displayName: string
+  uid: string
+  photoURL: string
+
   private loginStatus: LoginStatus
 
   constructor(private af: AngularFire) {
-    this.af.auth.subscribe((auth) => {
-      this.loginStatus = !!auth
+    this.af.auth.subscribe((res) => {
+      this.loginStatus = !!res
         ? LoginStatus.LoggedIn
         : LoginStatus.NotLoggedIn
 
       if (this.statusIsLoggedIn()) {
         this.whenLoggedIn.next(null)
+
+        this.displayName = res.auth.displayName
+        this.uid         = res.uid
+        this.photoURL    = res.auth.photoURL
+
       } else if (this.statusIsNotLoggedIn()) {
         this.whenLoggedOut.next(null)
       }
